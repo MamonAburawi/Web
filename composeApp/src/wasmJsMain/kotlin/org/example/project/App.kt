9b18b4d84d27ae4bba717bba74e428319698b9ef
Mamon Aburawi <mamon.aburawi@gmail.com>
2025-09-14 18:35:59 +0200

    add rest of project files

---
+++ composeApp/src/wasmJsMain/kotlin/org/example/project/App.kt
@@ -0,0 +1,36 @@
+package org.example.project
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import org.jetbrains.compose.resources.painterResource
+
+import kotlinproject.composeapp.generated.resources.Res
+import kotlinproject.composeapp.generated.resources.compose_multiplatform
+
+@Composable
+fun App() {
+    MaterialTheme {
+      Column(
+          modifier = Modifier.fillMaxSize(),
+          verticalArrangement = Arrangement.Center,
+          horizontalAlignment = Alignment.CenterHorizontally
+      ) {
+          Text(
+              text = "Home Screen",
+              fontSize = MaterialTheme.typography.bodyLarge.fontSize
+          )
+      }
+    }
+}
